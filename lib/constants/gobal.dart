c23fac8b98f47061c3b5a1c8d25fd0144a812ce1
masagatech <pratikway.90@gmail.com>
2022-05-22 13:31:29 +0530

    Update: update data

--- lib/constants/gobal.dart
+++ lib/constants/gobal.dart
@@ -1,8 +1,9 @@
 // ignore_for_file: constant_identifier_names
 
 class GlobalConstants {
-  //static const API_Host = "ec2-43-204-102-150.ap-south-1.compute.amazonaws.com:9090";
-  static const API_Host = "localhost:9090";
+  static const API_Host =
+      "ec2-43-204-102-150.ap-south-1.compute.amazonaws.com:9090";
+  //static const API_Host = "localhost:9090";
   static const API_Users_v1_saveStages = "/users/v1/saveStages";
 
   static const API_Users_v1_id = "/users/v1/";
