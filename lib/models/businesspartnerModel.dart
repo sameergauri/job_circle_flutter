774a5dda1a32b4dd59491a3adc6462b905fd12eb
zaid <zaid.ansari@ideas2tech.com>
2022-06-25 23:09:26 +0530

    commit my changes

---
+++ lib/models/businesspartnerModel.dart
@@ -0,0 +1,22 @@
+class BusinessPartnerFileUploadModel {
+  String? panCardLink;
+  String? panFileName;
+  String? panDateTime;
+  String? adharCardLink;
+  String? adharCardFileName;
+  String? adharCardDateTime;
+  String? cancelChequeLink;
+  String? cancelChequeFileName;
+  String? cancelChequeDateTime;
+  BusinessPartnerFileUploadModel({
+    this.panCardLink,
+    this.panFileName,
+    this.panDateTime,
+    this.adharCardLink,
+    this.adharCardFileName,
+    this.adharCardDateTime,
+    this.cancelChequeLink,
+    this.cancelChequeFileName,
+    this.cancelChequeDateTime,
+  });
+}
