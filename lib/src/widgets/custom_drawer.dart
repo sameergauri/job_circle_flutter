69a9b9393fc0922caff95b1eca23792730f65fe9
Gauri Mohd Sameer <gaurimohdsameer@gmail.com>
2026-01-25 13:43:31 +0530

    Uploaded to play store with v : 1.1.8

--- lib/src/widgets/custom_drawer.dart
+++ lib/src/widgets/custom_drawer.dart
@@ -339,7 +339,7 @@ class CustomDrawer extends StatelessWidget {
                     bottom: kToolbarHeight / 2,
                   ),
                   child: customText(
-                    title: 'Version 1.1.8',
+                    title: 'Version 1.1.9',
                     fontSize: 10,
                     fontWeight: FontWeight.normal,
                     fontStyle: FontStyle.italic,
